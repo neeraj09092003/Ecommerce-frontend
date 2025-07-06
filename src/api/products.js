bf93499b44e5193fe49c06cb5f6fe4b2aa38df6d
neeraj kanojia <neerajkanojia335@gmail.com>
2025-07-06 10:45:39 +0530

    Forth Commit

--- src/api/products.js
+++ src/api/products.js
@@ -2,11 +2,11 @@
 import API from "./axios";
 
 export const fetchProducts = async () => {
-  const { data } = await API.get("/products");
+  const { data } = await API.get("api/products");
   return data;
 };
 
 export const fetchProductById = async (id) => {
-  const { data } = await API.get(`/products/${id}`);
+  const { data } = await API.get(`api/products/${id}`);
   return data;
 };
